930bdd091a6f085c44776177a99b5c9687b43b72
baegteun <baegteun@gmail.com>
2023-04-25 11:17:02 +0900

    :sparkles: :: Color + ok

--- Projects/Core/DesignSystem/Sources/Color/Color+ok.swift
+++ Projects/Core/DesignSystem/Sources/Color/Color+ok.swift
@@ -1,5 +1,22 @@
 import SwiftUI
 
 public enum OKColorStyle {
-    
+    case primary(Primary)
+    case secondary(Secondary)
+    case blueGrayScale(BlueGrayScale)
+    case grayScale(GrayScale)
+    case etc(Etc)
+}
+
+public extension Color {
+    static func ok(_ style: OKColorStyle) -> Color {
+        switch style {
+        case let .primary(okColor as OKColorable),
+            let .secondary(okColor as OKColorable),
+            let .blueGrayScale(okColor as OKColorable),
+            let .grayScale(okColor as OKColorable),
+            let .etc(okColor as OKColorable):
+            return okColor.color
+        }
+    }
 }
